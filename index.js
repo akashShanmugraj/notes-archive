ebc492f19cce7276785637d3be64ecd2ef014759
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2023-10-29 14:00:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import path from 'path';
+
+const app  = express();
+
+app.get("/", (req, res) => {
+    res.sendfile('./index.html')
+})
+
+app.get('/carch/pipelining', (req, res) => {
+    res.sendfile('./pipelining.html')
+})
+
+app.listen(3000, () => {
+    console.log(`Server is listening on http://localhost:${process.env.PORT}`);
+  });
