97f4e42b14601ddb61b468c130b6963febdefb55
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2023-11-29 17:41:16 +0530

    added ECO and IITPKD Notes

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get('/carch/pipelining', (req, res) => {
     res.sendfile('./pipelining.html')
 })
 
+app.get('/cysec/iitpkd', (req, res) => {
+    res.sendfile('./iitpkd-cysec.html')
+})
+
+app.get('/eco/cost-revenue', (req, res) => {
+    res.sendfile('./cost-revenue.html')
+})
+
 app.listen(3030, () => {
-    console.log(`Server is listening on http://localhost:${process.env.PORT}`);
+    console.log(`Server is listening on http://localhost:3030`);
   });
